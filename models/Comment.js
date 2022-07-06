8746018eb6d9962194096456444c70976d870d46
cmskorcz <chaseskorey@icloud.com>
2022-07-06 19:11:08 -0400

    added comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,39 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isInt: true
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isInt: true
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Comment'
+    }
+);
+
+module.exports = Comment;
